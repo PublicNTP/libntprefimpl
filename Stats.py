27d93c4170e7659553b3b01ae1ba56c551bdaff3
Terry Ott <Terry.Ott@publicntp.org>
2017-10-26 03:45:54 +0000

    Pulling kerninfo and sysstat now

--- Stats.py
+++ Stats.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 import argparse
 import pprint
 import pexpect
+import json
 
 
 class NTPRefImplServerStats:
@@ -39,7 +40,7 @@ class NTPRefImplServerStats:
       'uptime'
     ]
 
-    print( "Stats string:\n{0}".format(statsString) )
+    #print( "Stats string:\n{0}".format(statsString) )
 
     # Break into array, one entry per line
     statLinesArray = statsString.splitlines()
@@ -100,11 +101,107 @@ class NTPRefImplServerStats:
 
     return self._parseInterfaceStatsString( ntpqChild.before.decode('UTF-8') )
 
+  def getSysstat(self):
+    ntpqChild = pexpect.spawn("ntpq -c sysstat {0}".format(self._hostname))
+    ntpqChild.expect(pexpect.EOF)
+
+    return self._createDictionary(ntpqChild.before.decode('UTF-8').splitlines())
+
+  def getKernelInfo(self):
+    ntpqChild = pexpect.spawn("ntpq -c kerninfo {0}".format(self._hostname))
+    ntpqChild.expect(pexpect.EOF)
+
+    linesToParse = ntpqChild.before.decode('UTF-8').splitlines()[1:]
+
+    # Fix calibration interval, missing a colon
+    for i in range(len(linesToParse)):
+      if linesToParse[i].startswith("calibration interval") is True:
+        linesToParse[i] = "calibration interval:{0}".format(
+          linesToParse[i][len("calibration interval")+1:])
+        #print( "after fix:\n{0}".format(linesToParse[i]))
+
+    return self._createDictionary(linesToParse)
+
+
+  def _createDictionary(self, colonDelineatedLines):
+
+    returnDict = {}
+    for currLine in colonDelineatedLines:
+      #print( "processing {0}".format(currLine))
+      (key, value) = currLine.split(':')
+      returnDict[key] = value.strip()
+
+      try:
+        returnDict[key] = int(returnDict[key])
+      except:
+        try:
+          returnDict[key] = float(returnDict[key])
+        except:
+          pass
+
+    return returnDict
+
+
   def getHostStats(self):
     hostStats = {
       'hostname': self._hostname,
-      'statistics': { 'interface': self.getInterfaceStats(), 'host': {} }
+      'statistics': { 
+        'interfaces': self.getInterfaceStats(), 
+        'host': {
+          'packets': { 
+            'sent': { 
+              'IPv4': {
+                'count'               : 0,
+                'packets_per_second'  : 0 
+              },
+              'IPv6': {
+                'count'               : 0,
+                'packets_per_second'  : 0
+              },
+              'total': {
+                'count'               : 0,
+                'packets_per_second'  : 0
+              }
+            }, 
+            'received': { 
+              'IPv4': {
+                'count'               : 0,
+                'packets_per_second'  : 0
+              },
+              'IPv6': {
+                'count'               : 0,
+                'packets_per_second'  : 0
+              },
+              'total': {
+                'count'               : 0,
+                'packets_per_second'  : 0
+              }
+            } 
+          },
+          'sysstat': self.getSysstat(),
+          'kernel': self.getKernelInfo()
+        }
+      }
     }
+
+    for interfaceName in hostStats['statistics']['interfaces']:
+      for interfaceAddress in hostStats['statistics']['interfaces'][interfaceName]:
+        currAddr = hostStats['statistics']['interfaces'][interfaceName][interfaceAddress]
+        for trafficDirection in [ 'sent', 'received' ]:
+          hostStats['statistics']['host']['packets'][trafficDirection][currAddr['ip_version']]['count'] += \
+            currAddr[trafficDirection]
+          hostStats['statistics']['host']['packets'][trafficDirection]['total']['count'] += \
+            currAddr[trafficDirection]
+
+    for trafficDirection in [ 'sent', 'received' ]:
+      for ipVersion in [ 'IPv4', 'IPv6' ]:
+        currStat = hostStats['statistics']['host']['packets'][trafficDirection][ipVersion]
+        currStat['packets_per_second'] = int(currStat['count'] / 
+          hostStats['statistics']['host']['sysstat']['uptime'])
+
+      hostStats['statistics']['host']['packets'][trafficDirection]['total']['packets_per_second'] = int( 
+         hostStats['statistics']['host']['packets'][trafficDirection]['total']['count'] / 
+         hostStats['statistics']['host']['sysstat']['uptime'] )
 
     return hostStats
 
@@ -130,4 +227,4 @@ if __name__ == "__main__":
   #print( statsObj )
   stats = statsObj.getHostStats()
 
-  print( "Stats: {0}".format(pprint.pformat(stats)) )
+  print( json.dumps(stats, sort_keys=True, indent=4) )
